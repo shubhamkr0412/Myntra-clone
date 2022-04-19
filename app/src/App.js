16b5051bdbe71a80db8086b4aaca7425bd8d8b04
Shubham Kumar <shubham35godda@gmail.com>
2022-04-19 09:40:19 +0530

    updated

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Data from './Components/Data';
 
 function App() {
   return (
     <div className="App">
+  <Data/>
      <h1>Hello</h1>
     </div>
   );
---
+++ app/src/Redux/action.js
@@ -0,0 +1,8 @@
+//actions basically return an object
+
+const storeData=(payload)=>({
+    type:"SRORE_DATA",
+    payload 
+}
+)
+export {storeData}
